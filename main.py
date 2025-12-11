87b8195e0ed8340cd6c644030f252d538658cc73
Patrick Perez <patrickperez1698@gmail.com>
2025-12-10 23:02:24 -0300

    feat: adicionar upload, leitura e exibição do arquivo

--- main.py
+++ main.py
@@ -1,6 +1,24 @@
-def main():
-    print("Hello from projeto-streamlit-teo!")
+import streamlit as st
+import pandas as pd
+st.set_page_config(page_title="Finanças", page_icon=":moneybag:")
 
 
-if __name__ == "__main__":
-    main()
+st.markdown('''
+            # Boas Vindas!
+            
+            ## Nosso APP Financeiro!
+
+            Espero que você curta a expericiência da nossa solução para organização financeira.
+''')
+# Widget de upload de dados
+file_upload = st.file_uploader(label="Faça upload dos dados aqui", type=['csv'])
+
+# Verifica se foi feito o upload do arquivo
+if file_upload:
+
+    # Leitura dos dados
+    df = pd.read_csv(file_upload)
+
+    # Exibição dos dados do App
+    columns_fmt = {'Valor' : st.column_config.NumberColumn(label="valor", format="R$ %f")}
+    st.dataframe(df, hide_index=True, column_config=columns_fmt)
