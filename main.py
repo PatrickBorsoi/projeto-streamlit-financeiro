21a58d97b7822a36dc933f4cb0f59aa314647cb3
Patrick Perez <patrickperez1698@gmail.com>
2025-12-11 22:13:49 -0300

    feat: adicionar instituicoes, expanders e graficos

--- main.py
+++ main.py
@@ -18,7 +18,22 @@ if file_upload:
 
     # Leitura dos dados
     df = pd.read_csv(file_upload)
+    #formatando a data
+    df['Data'] = pd.to_datetime(df['Data'], format='%d/%m/%Y').dt.date
+
 
     # Exibição dos dados do App
+    exp1 = st.expander('Dados Brutos')
+     
     columns_fmt = {'Valor' : st.column_config.NumberColumn(label="valor", format="R$ %f")}
-    st.dataframe(df, hide_index=True, column_config=columns_fmt)
+    exp1.dataframe(df, hide_index=True, column_config=columns_fmt)
+    
+    # Visão Instituição
+    exp2 = st.expander('Instituições')
+    df_instituicao = df.pivot_table(index='Data', columns='Instituição', values='Valor')
+    exp2.dataframe(df_instituicao)
+    exp2.line_chart(df_instituicao)
+    
+    # Ultima data de dados
+    last_dt = df_instituicao.sort_index().iloc[-1]
+    exp2.bar_chart(last_dt)
